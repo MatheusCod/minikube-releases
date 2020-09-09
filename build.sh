5e2aa9874aa7820da9868e3322bc052552fd9376
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-09 18:43:40 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 FTP_HOST='oplab9.parqtec.unicamp.br'
 LOCALPATH=$GOPATH/src/
-REMOTEPATH='/ppc64el/glide'
+REMOTEPATH='/ppc64el/minikube'
 ROOTPATH="~/rpmbuild/RPMS/ppc64le"
-REPO1="/repository/debian/ppc64el/minikube"
-REPO2="/repository/rpm/ppc64le/minikube"
+#REPO1="/repository/debian/ppc64el/minikube"
+#REPO2="/repository/rpm/ppc64le/minikube"
+REPO1="/test/matheus"
+REPO2="/test/matheus"
 github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
@@ -35,11 +37,11 @@ then
     sudo ./empacotar-rpm.sh minikube minikube-$github_version $github_version " " "minikube implements a local Kubernetes cluster on macOS, Linux, and Windows"
     if [[ $github_version > $ftp_version ]]
     then
-        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minikube/latest/ $LOCALPATH/k8s.io/minikube/out/minikube_$github_version"
-        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minikube/latest/minikube_$del_version"
+        #sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minikube/latest/ $LOCALPATH/k8s.io/minikube/out/minikube_$github_version"
+        #sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minikube/latest/minikube_$del_version"
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minikube-$github_version-ppc64le.deb"
         sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO2 $ROOTPATH/minikube-$github_version-1.ppc64le.rpm"
     fi
-    sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minikube/ /home/travis/gopath/src/k8s.io/minikube/out/minikube_$gtihub_version"
+    #sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minikube/ /home/travis/gopath/src/k8s.io/minikube/out/minikube_$gtihub_version"
     #sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minikube/minikube_$del_version"
 fi
